b38b324d4015dc959dc9d86915b0765b3c90695d
Yilai Li <44369625+yilaili@users.noreply.github.com>
2020-07-17 00:45:16 -0400

    lower num of nodes

--- run_submission.sh
+++ run_submission.sh
@@ -14,10 +14,10 @@ do
   for tau2_fudge in 1 2 8 24
   do
     python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 3 --tau2_fudge $tau2_fudge --nodes 1 &
-    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 10 --tau2_fudge $tau2_fudge --nodes 2 &
-    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 25 --tau2_fudge $tau2_fudge --nodes 4 &
-    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 50 --tau2_fudge $tau2_fudge --nodes 5 &
-    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 100 --tau2_fudge $tau2_fudge --nodes 8 &
+    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 10 --tau2_fudge $tau2_fudge --nodes 1 &
+    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 25 --tau2_fudge $tau2_fudge --nodes 1 &
+    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 50 --tau2_fudge $tau2_fudge --nodes 2 &
+    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 100 --tau2_fudge $tau2_fudge --nodes 5 &
     wait
   done
 done
@@ -28,10 +28,10 @@ do
   for tau2_fudge in 1 2 8 24
   do
     python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 3 --tau2_fudge $tau2_fudge --ctf --nodes 1 &
-    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 10 --tau2_fudge $tau2_fudge --ctf --nodes 2 &
-    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 25 --tau2_fudge $tau2_fudge --ctf --nodes 4 &
-    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 50 --tau2_fudge $tau2_fudge --ctf --nodes 5 &
-    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 100 --tau2_fudge $tau2_fudge --ctf --nodes 8 &
+    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 10 --tau2_fudge $tau2_fudge --ctf --nodes 1 &
+    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 25 --tau2_fudge $tau2_fudge --ctf --nodes 1 &
+    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 50 --tau2_fudge $tau2_fudge --ctf --nodes 2 &
+    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 100 --tau2_fudge $tau2_fudge --ctf --nodes 5 &
     wait
   done
 done
@@ -42,10 +42,10 @@ do
   for tau2_fudge in 1 2 8 24
   do
     python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 3 --tau2_fudge $tau2_fudge --ctf --ctf_intact_first_peak --nodes 1 &
-    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 10 --tau2_fudge $tau2_fudge --ctf --ctf_intact_first_peak --nodes 2 &
-    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 25 --tau2_fudge $tau2_fudge --ctf --ctf_intact_first_peak --nodes 4 &
-    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 50 --tau2_fudge $tau2_fudge --ctf --ctf_intact_first_peak --nodes 5 &
-    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 100 --tau2_fudge $tau2_fudge --ctf --ctf_intact_first_peak --nodes 8 &
+    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 10 --tau2_fudge $tau2_fudge --ctf --ctf_intact_first_peak --nodes 1 &
+    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 25 --tau2_fudge $tau2_fudge --ctf --ctf_intact_first_peak --nodes 1 &
+    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 50 --tau2_fudge $tau2_fudge --ctf --ctf_intact_first_peak --nodes 2 &
+    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 100 --tau2_fudge $tau2_fudge --ctf --ctf_intact_first_peak --nodes 5 &
     wait
   done
 done
@@ -57,10 +57,10 @@ do
   for tau2_fudge in 1 2 8 24
   do
     python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 3 --tau2_fudge $tau2_fudge --zero_mask --nodes 1 &
-    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 10 --tau2_fudge $tau2_fudge --zero_mask --nodes 2 &
-    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 25 --tau2_fudge $tau2_fudge --zero_mask --nodes 4 &
-    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 50 --tau2_fudge $tau2_fudge --zero_mask --nodes 5 &
-    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 100 --tau2_fudge $tau2_fudge --zero_mask --nodes 8 &
+    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 10 --tau2_fudge $tau2_fudge --zero_mask --nodes 1 &
+    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 25 --tau2_fudge $tau2_fudge --zero_mask --nodes 1 &
+    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 50 --tau2_fudge $tau2_fudge --zero_mask --nodes 2 &
+    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 100 --tau2_fudge $tau2_fudge --zero_mask --nodes 5 &
     wait
   done
 done
@@ -71,10 +71,10 @@ do
   for tau2_fudge in 1 2 8 24
   do
     python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 3 --tau2_fudge $tau2_fudge --ctf --zero_mask --nodes 1 &
-    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 10 --tau2_fudge $tau2_fudge --ctf --zero_mask --nodes 2 &
-    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 25 --tau2_fudge $tau2_fudge --ctf --zero_mask --nodes 4 &
-    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 50 --tau2_fudge $tau2_fudge --ctf --zero_mask --nodes 5 &
-    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 100 --tau2_fudge $tau2_fudge --ctf --zero_mask --nodes 8 &
+    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 10 --tau2_fudge $tau2_fudge --ctf --zero_mask --nodes 1 &
+    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 25 --tau2_fudge $tau2_fudge --ctf --zero_mask --nodes 1 &
+    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 50 --tau2_fudge $tau2_fudge --ctf --zero_mask --nodes 2 &
+    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 100 --tau2_fudge $tau2_fudge --ctf --zero_mask --nodes 5 &
     wait
   done
 done
@@ -85,10 +85,10 @@ do
   for tau2_fudge in 1 2 8 24
   do
     python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 3 --tau2_fudge $tau2_fudge --ctf --ctf_intact_first_peak --zero_mask --nodes 1 &
-    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 10 --tau2_fudge $tau2_fudge --ctf --ctf_intact_first_peak --zero_mask --nodes 2 &
-    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 25 --tau2_fudge $tau2_fudge --ctf --ctf_intact_first_peak --zero_mask --nodes 4 &
-    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 50 --tau2_fudge $tau2_fudge --ctf --ctf_intact_first_peak --zero_mask --nodes 5 &
-    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 100 --tau2_fudge $tau2_fudge --ctf --ctf_intact_first_peak --zero_mask --nodes 8 &
+    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 10 --tau2_fudge $tau2_fudge --ctf --ctf_intact_first_peak --zero_mask --nodes 1 &
+    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 25 --tau2_fudge $tau2_fudge --ctf --ctf_intact_first_peak --zero_mask --nodes 1 &
+    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 50 --tau2_fudge $tau2_fudge --ctf --ctf_intact_first_peak --zero_mask --nodes 2 &
+    python /lsi/groups/mcianfroccolab/yilai/codes/cryoEDU_2D/submit_2DClass.py -i job014/particles_1.star -d $diam -K 100 --tau2_fudge $tau2_fudge --ctf --ctf_intact_first_peak --zero_mask --nodes 5 &
     wait
   done
 done
